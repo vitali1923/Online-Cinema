f6768330433faf67e37471eda06f3a5ba6047f86
vitali1923 <78555387+vitali1923@users.noreply.github.com>
2021-10-11 14:49:49 +0300

    Закончили демо версию онлайн кинотеатра
    Добавили адаптив, мобильную версию, подключили библиотеку для видео

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,5 +1,7 @@
 const schemeSvg = document.querySelector(".scheme-svg");
 const totalPriceTag = document.querySelector(".price-total");
+const menuButton = document.querySelector(".m-menu");
+const menu = document.querySelector(".menu");
 let cost = 500;
 let totalPrice = 0;
 schemeSvg.addEventListener("click", (event)=>{
@@ -10,3 +12,7 @@ schemeSvg.addEventListener("click", (event)=>{
   totalPriceTag.textContent = totalPrice;
  }
 });
+menuButton.addEventListener("click",()=>{
+  console.log("Кликнули по меню");
+  menu.classList.toggle("is-open");
+})
